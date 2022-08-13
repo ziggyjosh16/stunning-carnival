993ab50b8b7a8e2f13057911977ac798fb7a33b5
Josh Sharkey <ziggyjosh16@yahoo.com>
2022-08-13 15:55:33 -0400

    change service to imperial

--- src/weatherController.py
+++ src/weatherController.py
@@ -1,19 +1,19 @@
-from config import appid
 import requests
-appid = ""
 
 
 def get_air_temp(user_ip):
     url1 = "http://ip-api.com/json/{}".format(
-        user_ip) if user_ip else "http://ip-api.com/json"
+        user_ip) if user_ip and user_ip not in ["0.0.0.0", "::1", "127.0.0.1"] else "http://ip-api.com/json"
     r = requests.get(url1).json()
+    # print(r)
     location = {
-        "lat": r.lat,
-        "lon": r.lon
+        "lat": r['lat'],
+        "lon": r['lon'],
+        "city": r['city'],
+        "region": r['region'],
+        "countryCode": r['countryCode']
     }
-    url2 = "https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&exclude=minutely,hourly,daily,alerts&appid=95a77bc86522876229ff64adad24253e \
-    ".format(location.lat, location.lon)
-    r = requests.get(url1).json()
-
-    w_data = {}
-    return 0
+    url2 = "https://api.openweathermap.org/data/2.5/weather?q={},{},{} \
+        &appid=95a77bc86522876229ff64adad24253e&units=imperial".format(location['city'], location['region'], location['countryCode'])
+    r2 = requests.get(url2).json()
+    return r2.get('main', {}).get("temp", 0)
